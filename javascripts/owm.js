bb2045bc393d0f84a24d08ed4673da472666c778
holmanhub <holman.eric85@gmail.com>
2017-10-23 11:45:59 -0500

    added domstring and update js files and add scss files

--- javascripts/owm.js
+++ javascripts/owm.js
@@ -1,13 +1,13 @@
 "use strict";
 
 let owmKey;
-//const dom = require('./dom');
+const dom = require('./dom');
 const events = require('./events');
 
 const searchOWM = (zip) => {
 	// promise search zip codes
 	return new Promise((resolve, reject) => {
-		$.ajax(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${owmKey}`).done((data) => {
+		$.ajax(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${owmKey}&units=imperial`).done((data) => {
 			console.log(data);
 			resolve(data.results);
 		}).fail((error) => {
@@ -32,9 +32,9 @@ const setKey = (apiKey) => {
 	console.log("owmKey", owmKey);
 };
 
-// const showResults = (weatherArray) => {
-// 	dom.clearDom();
-// 	dom.domString(weatherArray);
-// };
+const showResults = (weatherArray) => {
+	dom.clearDom();
+	dom.domString(weatherArray);
+};
 
 module.exports = {setKey, searchOWM};
